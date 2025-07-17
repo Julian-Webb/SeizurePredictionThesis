369eaef2e30aa543d4ad52c16e13e831285ef21c
Julian-Webb <julian.webb00+github@gmail.com>
2025-07-17 17:09:56 +0200

    Added server test script and sandbox folder

--- sandbox/show_file_names.py
+++ sandbox/show_file_names.py
@@ -2,7 +2,8 @@
 import os
 
 if __name__ == '__main__':
+    print(f'{os.getcwd()=}')
     path = r'/data/datasets/20240201_UNEEG_ForMayo/B52K3P3G/V5a'
     files = os.listdir(path)
-    # files = os.listdir('.')
+    print(f'Files in {path}:')
     print(files)
