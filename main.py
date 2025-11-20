5f4e09a0972366998889d20161ea270eeea9d1e3
Julian Webb <julian.webb00+github@gmail.com>
2025-11-20 23:02:36 +0100

    Stopped tracking .idea

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
+import logging
+
 from data_cleaning.main import data_cleaning
 from scripts import copy_uneeg_data
 
 def main():
+    logging.basicConfig(level=logging.INFO, format='[%(levelname)s] %(message)s')
     copy_uneeg_data.main()
     data_cleaning(ask_confirm=False)
 
