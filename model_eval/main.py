6125e4cc9269487ff35434ea5e8c015d896f5579
Julian-Webb <julian.webb00+github@gmail.com>
2026-03-16 11:52:32 +0100

    Added main entry point for model evaluation pipeline

---
+++ model_eval/main.py
@@ -0,0 +1,34 @@
+import logging
+
+from config import PatientDir
+from model_eval import calc_segment_probabilities, clips, event_based_metrics, eval_models
+from utils.utils import FunctionTimer
+
+
+def model_eval(
+        pdirs: list[PatientDir],
+        available_gpus: list[int],
+):
+    """
+    Run all the model evaluation steps.
+    """
+    logging.basicConfig(level=logging.INFO, format='[%(levelname)s] %(message)s')
+
+    with FunctionTimer("====== Calculating Segment Probabilities ======="):
+        calc_segment_probabilities.main(pdirs, serial_processing=False, available_gpus=available_gpus)
+
+    with FunctionTimer('====== Computing Clips ======'):
+        clips.main(pdirs)
+
+    with FunctionTimer('====== Computing Event-Based Metrics ======'):
+        event_based_metrics.calc_metrics(pdirs)
+
+    with FunctionTimer('====== Evaluating Models ======'):
+        eval_models.main(pdirs)
+
+
+if __name__ == '__main__':
+    model_eval(
+        PatientDir.patient_dirs(),
+        available_gpus=[0, 1, 2, 3]
+    )
